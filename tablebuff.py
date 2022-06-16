c192661dc65aab455a81bf7211ba948e69a3aaf1
Luis Henrique Arandas <luis.arandas@gmail.com>
2022-06-16 01:30:19 +0100

    Update tablebuff.py

--- tablebuff.py
+++ tablebuff.py
@@ -8,10 +8,12 @@ import pyo as pyo
 import dearpygui.dearpygui as dpg
 from os import listdir, path
 import time
+import math
 
 audio_server = pyo.Server()
 audio_server.boot()
 audio_table = pyo.SndTable()
+audio_table.append("/Users/luisarandas/github/cmte/media/concrete.wav")
 
 current_selected_file = ""
 audio_file_extensions = [".mp3", ".flac", ".aiff", ".wav", ".ogg", ".alac", ".dsd"]
@@ -89,6 +91,34 @@ def add_audio_folder(sender, app_data):
     print(app_data)
 
 
+sindatax = []
+sindatay = []
+def generate_plots():
+    audio_table.normalize()
+    yo_y = audio_table.getTable() # all=False
+    print("get buffer ", len(yo_y))
+    start_distance = 0.0
+    x_data = [0.0] # number of samples and define distance
+    for i in range(len(yo_y)):
+        start_distance = start_distance + 1
+        x_data.append(start_distance)
+    
+    print("x data ", yo_y)
+    y_data = yo_y
+
+    global sindatax
+    sindatax = x_data
+    sindatay = yo_y
+
+
+    # global sindatax
+    # global sindatay
+    # for i in range(50):
+    #     sindatax.append(i/100) # just make steps of 0.01
+    #     sindatay.append(0.5 + 0.5 * math.sin(50*i/100))
+    
+generate_plots()
+
 
 with dpg.window(tag="mainwindow"):
 
@@ -116,13 +146,33 @@ with dpg.window(tag="mainwindow"):
         # dpg.add_listbox(tag="table_listbox", label='table_listbox', callback=audio_table_callback, num_items=len(current_audio_table), width=400)
         dpg.add_text(current_audio_table_text, tag="current_audios_in_the_table", wrap=400)
 
-    with dpg.window(tag="plotting_viewer", width=400, height=250, menubar=False, no_title_bar=True, no_move=True, no_resize=True):
+    with dpg.window(tag="plotting_viewer", width=550, height=300, menubar=False, no_title_bar=True, no_move=True, no_resize=True):
         dpg.set_item_pos("plotting_viewer", [425., 5.])
         dpg.add_text("plotting table")
         dpg.add_separator()
         dpg.add_spacer()
-        
+        # with dpg.subplots(3, 3, label="", width=-1, height=-1, row_ratios=[5.0, 1.0, 1.0], column_ratios=[5.0, 1.0, 1.0]) as subplot_id:
+        #     for i in range(9):
+        #         with dpg.plot(no_title=True):
+        #             dpg.add_plot_axis(dpg.mvXAxis, label="", no_tick_labels=True)
+        #             with dpg.plot_axis(dpg.mvYAxis, label="", no_tick_labels=True):
+        #                 dpg.add_line_series(sindatax, sindatay, label="0.5 + 0.5 * sin(x)")
 
+        with dpg.plot(label="", height=-1, width=-1, anti_aliased=True):
+        
+            # dpg.add_plot_legend()
+
+            dpg.add_plot_axis(dpg.mvXAxis, label="")
+            with dpg.plot_axis(dpg.mvYAxis, label=""):
+
+                # series 1
+                dpg.add_line_series(sindatax, sindatay, label="series 1")
+                dpg.add_button(label="Delete Series 1", user_data = dpg.last_item(), parent=dpg.last_item(), callback=lambda s, a, u: dpg.delete_item(u))
+
+                # series 2
+                dpg.add_line_series(sindatax, sindatay, label="series 2")
+                dpg.add_button(label="Delete Series 2", user_data = dpg.last_item(), parent=dpg.last_item(), callback=lambda s, a, u: dpg.delete_item(u))
+                        
 
 dpg.setup_dearpygui()
 dpg.show_viewport()
