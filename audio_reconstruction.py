274b26b3f9ec70f4df317659e377fd974c48d789
Luis Henrique Arandas <luis.arandas@gmail.com>
2022-06-28 22:45:54 +0100

    Update .gitignore, audio_reconstruction.py, and example.wav

--- audio_reconstruction.py
+++ audio_reconstruction.py
@@ -5,3 +5,19 @@
 # check
 # https://kuleshov.github.io/audio-super-res/
 # https://github.com/shun60s/FFT-Wav-UpSampling2
+
+# import soundfile as sf
+# from speechbrain.pretrained import WaveformEnhancement
+
+# enhance_model = WaveformEnhancement.from_hparams(
+#     source="speechbrain/mtl-mimic-voicebank",
+#     savedir="pretrained_models/mtl-mimic-voicebank",
+# )
+
+# enhanced = enhance_model.enhance_file("speechbrain/mtl-mimic-voicebank/example.wav")
+
+# sf.write("output.wav", enhanced.unsqueeze(0)[0].cpu, 16000, 'PCM_24')
+
+# # torchaudio.save("enhanced.wav", enhanced.unsqueeze(0).cpu, 16000)
+
+# print("yo")
